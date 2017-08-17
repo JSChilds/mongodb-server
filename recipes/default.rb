6ebfa73aab8529a1f8b1ff65f6e6ed16066db5f1
Tech-A40 <Tech-A40@Tech-A40s-MacBook-Air.local>
2017-08-17 11:55:09 +0100

    Initial commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,40 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+apt_repository 'name' do
+
+	keyserver "hkp://keyserver.ubuntu.com:80"
+	key "EA312927"
+	components ["multiverse"]
+	distribution "xenial/mongodb-org/3.2"
+	uri "http://repo.mongodb.org/apt/ubuntu"
+
+end
+
+package 'mongodb-org'
+
+template "/lib/systemd/system/mongod.service" do
+
+	source 'mongod.service.erb'
+	owner 'root'
+	group 'root'
+	mode '755'
+	notifies :restart, 'service[mongod]'
+
+end
+
+template "/etc/mongod.conf" do
+
+	source 'mongod.conf.erb'
+	owner 'root'
+	group 'root'
+	mode '755'
+	notifies :restart, 'service[mongod]'
+
+end
+
+service 'mongod' do
+	supports :restart => true, :reload => true, :start => true
+	action [:enable, :start]
+end
